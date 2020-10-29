92f60df58d4f4da7ab3f0327d625ce9f365a26c2
alycia <alycialwang@gmail.com>
2020-10-29 13:45:16 -0400

    🍰 Frontend File Tree Initialized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import Test from "./Test";
+import { ProtectedRoute, PrivateRoute } from "./Utility/route-utility"
+import LoginPage from "./Login/LoginPage";
+import HomePage from "./HomePage/HomePage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" render={() => <Test />} />
+        <ProtectedRoute path="/" exact={true} needLogin={needLogin} component={LoginPage} />
+        <PrivateRoute path="/" exact={true} needLogin={needLogin} component={HomePage} />
+        <Redirect to="/" />
       </Switch>
     </BrowserRouter>
   );
--- src/context/BuildContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const BuildContext = createContext();
-
-export default BuildContext;
