2fc920deddfa654bef3a677158da43819ce4a91e
alycia <alycialwang@gmail.com>
2020-10-29 18:41:05 -0400

    🍰 Added Utility Actions

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import authentication from "./reducers/authentication";
+import utility from "./reducers/utility";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducer = combineReducers({
   authentication,
+  utility
 });
 
 const configureStore = (initialState) => {
