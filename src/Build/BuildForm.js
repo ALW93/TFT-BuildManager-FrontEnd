1d5743c30b075d12410e56f01b7fc5ac69770fa9
alycialwang@gmail.com <alycialwang@gmail.com>
2020-10-31 15:07:44 -0400

    🍰 Holy Moly this Build Form is difficult but so fun

--- src/Build/BuildForm.js
+++ src/Build/BuildForm.js
@@ -3,6 +3,9 @@ import { TextField, Button } from "@material-ui/core";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import { champions } from "../Utility/game-data";
+import { ICON_IMG_API } from "../config";
+import "./BuildForm.css";
 
 const BuildForm = (props) => {
   const [title, setTitle] = useState("");
@@ -13,12 +16,18 @@ const BuildForm = (props) => {
 
   const handleSubmit = "";
 
+  const updateTeam = () => {};
+
   const updateItem = (cb) => (e) => {
     return cb(e.target.value);
   };
 
+  const handleSelect = (e) => {
+    console.log(e.target.getAttribute("id"));
+  };
+
   return (
-    <>
+    <div className="formContainer">
       <form>
         <TextField
           type="text"
@@ -28,18 +37,30 @@ const BuildForm = (props) => {
         />
         <Select
           placeholder="Choose Playstyle"
-          value={playstyle}
-          onChange={updateItem(playstyle)}
+          id={playstyle}
+          onChange={updateItem(setPlaystyle)}
         >
           <MenuItem value={"Standard"}>Standard</MenuItem>
           <MenuItem value="Slow Roll">Slow Roll</MenuItem>
           <MenuItem value="Re-Roll">Re-Roll</MenuItem>
           <MenuItem value="Fast 8">Fast 8</MenuItem>
         </Select>
-        <TextareaAutosize />
+        <div className="champion-grid-container">
+          {Object.keys(champions).map((c) => (
+            <img
+              key={champions[c]}
+              id={champions[c]}
+              className="champIcon"
+              src={`${ICON_IMG_API}${c}.png`}
+              onClick={handleSelect}
+            />
+          ))}
+        </div>
+        <div className="team-grid-container">{JSON.stringify(team)}</div>
+        <TextareaAutosize value={notes} onChange={updateItem(setNotes)} />
         <Button>Create Build</Button>
       </form>
-    </>
+    </div>
   );
 };
 
--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export const TFT_BASE = "http://localhost:8000";
 export const TFT_API = `${TFT_BASE}/api`;
 export const IMG_API = "https://tft-buildmanager.s3.amazonaws.com/Champions";
+export const ICON_IMG_API =
+  "https://tft-buildmanager.s3.amazonaws.com/champion-icon/TFT4_";
 export const demoToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo0LCJlbWFpbCI6Imh1YW5nQHlhaG9vLmNvbSJ9LCJpYXQiOjE2MDM5MTMwMjIsImV4cCI6MTYwNDUxNzgyMn0.cuA8QpHouWmmViXlaDUpgxqndMylBmXgLtic0l1eINU";
