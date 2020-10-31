c9473d50384a7844248387c396e0e2072fdf4b99
alycialwang@gmail.com <alycialwang@gmail.com>
2020-10-30 23:07:02 -0400

    🍰 Created Champion Image for AWS Migration

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -11,6 +11,8 @@ const moreTitle = "View All Meta Builds";
 const HomePage = () => {
   const [data, setData] = useState([]);
 
+  console.log(data);
+
   useEffect(() => {
     async function fetchData() {
       const buildArray = await getEditorBuilds();
