0da068a20ba61eb3d89f9e8606568b0da4216b3a
alycialwang@gmail.com <alycialwang@gmail.com>
2020-11-01 18:13:58 -0500

    🍰 Build Form 80% Style

--- src/Utility/routes.js
+++ src/Utility/routes.js
@@ -1,23 +1,13 @@
 import React from "react";
-import BuildContainer from "../Build/Build-Container";
 import BuildView from "../Build/BuildView";
+import BuildForm from "../Build/BuildForm";
 import { Route } from "react-router-dom";
 import Profile from "../Profile/ProfilePage";
 
 const Routes = () => (
   <>
     <Route exact path="/build/id/:id" type="view" component={BuildView} />
-    <Route
-      exact
-      path="/build-create"
-      component={() => <BuildContainer type={"create"} />}
-    />
-    <Route
-      exact
-      path="/build-edit"
-      type="edit"
-      component={() => <BuildContainer type={"edit"} />}
-    />
+    <Route exact path="/build-create" component={BuildForm} />s
     <Route exact path="/profile/:id" component={Profile} />
     <Route path="/profile/:id/bookmarks" component={Profile} />
   </>
