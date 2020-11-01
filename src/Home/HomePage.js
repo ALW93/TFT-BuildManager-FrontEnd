466665645b19e1a653e421ff048f951271cfb7e8
alycialwang@gmail.com <alycialwang@gmail.com>
2020-11-01 13:10:37 -0500

    🍰 Main Page Elements All There

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -3,13 +3,18 @@ import "./HomePage.css";
 import TopBar from "../shared_components/TopBar";
 import SmallCard from "../shared_components/SmallCard";
 import { TFT_BASE, IMG_API } from "../config";
-import { getEditorBuilds, parseCardData } from "../Fetches/fetches";
+import {
+  getAllbuilds,
+  getEditorBuilds,
+  parseCardData,
+} from "../Fetches/fetches";
 
 const image = `${IMG_API}/Azir.jpg`;
 const moreTitle = "View All Meta Builds";
 
 const HomePage = () => {
   const [data, setData] = useState([]);
+  const [allData, setAllData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -22,21 +27,52 @@ const HomePage = () => {
           setData((data) => [...data, info]);
         }
       });
+
+      const allArray = await getAllbuilds();
+      allArray.forEach(async (b, i) => {
+        const response = await fetch(`${TFT_BASE}/builds/id/${b}`);
+        if (response.ok) {
+          const allDetails = await response.json();
+          const allInfo = await parseCardData(allDetails.build);
+          setAllData((allData) => [...allData, allInfo]);
+        }
+      });
     }
+
     fetchData();
   }, []);
 
-  const Cards = () => {
-    return data.map((e) => (
-      <SmallCard
-        key={e.id}
-        id={e.id}
-        image={e.image}
-        title={e.title}
-        author={e.author}
-        authorId={e.authorId}
-      />
-    ));
+  const AllCards = () => {
+    return allData.map((e) => {
+      return (
+        <SmallCard
+          key={e.id}
+          id={e.id}
+          image={e.image}
+          title={e.title}
+          author={e.author}
+          authorId={e.authorId}
+        />
+      );
+    });
+  };
+
+  const MetaCards = () => {
+    let counter = 0;
+    return data.map((e) => {
+      if (counter === 5) return;
+      counter++;
+      return (
+        <SmallCard
+          key={e.id}
+          id={e.id}
+          image={e.image}
+          title={e.title}
+          author={e.author}
+          authorId={e.authorId}
+        />
+      );
+    });
   };
 
   return (
@@ -45,9 +81,13 @@ const HomePage = () => {
 
       <div className="BuildContainer__Meta">
         <h1 className="metaTitle">Meta Builds for Patch 10.22</h1>
-        <div className="BuildContainer__Meta--Carousel">
-          <Cards />
+        <div className="BuildContainer__Carousel">
+          <MetaCards />
           <SmallCard image={image} title={moreTitle} utility={true} />
+        </div>
+        <h1 className="metaTitle">Community Builds</h1>
+        <div className="BuildContainer__Carousel">
+          <AllCards />
         </div>
       </div>
     </div>
