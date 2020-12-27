1475f50cfce8e49a6f3aeb5c4797fad7fdd321e9
alycialwang@gmail.com <alycialwang@gmail.com>
2020-12-26 20:03:55 -0500

    Filter By Cost

--- src/TestBuilder/SelectionPool.js
+++ src/TestBuilder/SelectionPool.js
@@ -1,23 +1,60 @@
 import React, { useState, useEffect } from "react";
-import { champions, traits } from "../set4/set4";
+import { TFT_API } from "../config";
+import { RadioGroup, FormControlLabel, Radio } from "@material-ui/core";
 import "./TestBuilder.css";
 
 const SelectionPool = ({ onDragOver, onDropDelete, onDragStart }) => {
-  const [filter, setFilter] = useState([]);
+  const [champions, setChampions] = useState([]);
+  const [filter, setFilter] = useState({ cost: null, trait: null });
 
   useEffect(() => {
-    //
+    (async () => {
+      let data;
+      if (filter.cost && !filter.trait) {
+        data = await fetch(`${TFT_API}/champions/filter/cost/${filter.cost}`);
+      }
+      if (!filter.cost && filter.trait) {
+        data = await fetch(`${TFT_API}/champions/filter/trait/${filter.trait}`);
+      }
+      if (filter.cost && filter.trait) {
+        data = await fetch(
+          `${TFT_API}/champions/filter/cost/${filter.cost}/trait/${filter.trait}`
+        );
+      }
+      if (!filter.cost && !filter.trait) {
+        data = await fetch(`${TFT_API}/champions`);
+      }
+      const parsed = await data.json();
+      setChampions(parsed);
+    })();
   }, [filter]);
+
+  const costHandler = (e) => {
+    setFilter({ ...filter, cost: e.target.value });
+    console.log(filter);
+  };
 
   return (
     <div>
+      <RadioGroup
+        aria-label="gender"
+        name="gender1"
+        value={filter.cost}
+        onChange={costHandler}
+      >
+        <FormControlLabel value="" control={<Radio />} label="None" />
+        <FormControlLabel value="1" control={<Radio />} label="One" />
+        <FormControlLabel value="2" control={<Radio />} label="Two" />
+        <FormControlLabel value="3" control={<Radio />} label="Three" />
+        <FormControlLabel value="4" control={<Radio />} label="Four" />
+        <FormControlLabel value="5" control={<Radio />} label="Five" />
+      </RadioGroup>
       <div
         className="character__selection"
         onDragOver={(e) => onDragOver(e)}
         onDrop={(e) => onDropDelete(e)}
       >
         {champions.map((champion) => {
-          console.log(champion);
           return (
             <img
               onDragStart={(e) => onDragStart(e, champion.championId, null)}
