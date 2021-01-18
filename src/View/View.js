2b49a69917d2202d3d8aab86960684ab0b16bdda
alycialwang@gmail.com <alycialwang@gmail.com>
2021-01-17 21:58:59 -0500

    Resolved Luxon Issue for Deploy

--- src/View/View.js
+++ src/View/View.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { TFT_BASE } from "../config";
-import { DateTime } from "luxon";
 import ViewBoard from "./ViewBoard";
 
 const View = ({ match }) => {
@@ -8,10 +7,6 @@ const View = ({ match }) => {
   const [board, setBoard] = useState([]);
   const [author, setAuthor] = useState({});
   const [editor, showEditor] = useState(true);
-
-  const created = DateTime.fromISO(board.createdAt).toLocaleString(
-    DateTime.DATE_MED
-  );
 
   useEffect(() => {
     (async () => {
@@ -28,7 +23,7 @@ const View = ({ match }) => {
       <h1>Board</h1>
       <h2>{board.title}</h2>
       <h2>Votes {board.votes || 0}</h2>
-      <h3>Last Updated {created}</h3>
+      <h3>Last Updated </h3>
       <h3>Created By {author.username}</h3>
       <ViewBoard data={board.grid} />
     </div>
