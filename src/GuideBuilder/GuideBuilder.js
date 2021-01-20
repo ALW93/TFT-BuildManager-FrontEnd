64ec3b7aee0ec58b5961e8c22239e1f7305b159d
alycialwang@gmail.com <alycialwang@gmail.com>
2021-01-20 16:48:46 -0500

    Guide Boards Posting in order

--- src/GuideBuilder/GuideBuilder.js
+++ src/GuideBuilder/GuideBuilder.js
@@ -1,22 +1,31 @@
 import React, { useState, createRef } from "react";
 import Editor from "../shared_components/Editor";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Dialog from "@material-ui/core/Dialog";
 import NewBuilder from "../NewBuilder/NewBuilder";
 import ViewBoard from "../View/ViewBoard";
 import { TFT_BASE } from "../config";
+import { addBoard } from "../store/actions/editor";
+import "./GuideBuilder.css";
 
 const GuideBuilder = () => {
   const [saved, initSave] = useState(false);
   const [builder, showBuilder] = useState(false);
   const [collection, showCollection] = useState(false);
   const boards = useSelector((state) => state.editor.boards);
+  const boardCollection = useSelector((state) => state.info.boards);
   const editRef = createRef();
+  const dispatch = useDispatch();
 
   const setSave = (e) => {
     console.log("saving");
     e.preventDefault();
     initSave(true);
+  };
+
+  const addExisting = (data) => {
+    dispatch(addBoard(data));
+    showCollection(false);
   };
 
   const submitHandler = async (e) => {
@@ -34,10 +43,10 @@ const GuideBuilder = () => {
         guide: saveContent,
         title: title.value || "untitled",
         id: 1,
+        boards: boards,
       }),
     });
     const data = await response.json();
-    console.log(data);
   };
 
   return (
@@ -52,10 +61,10 @@ const GuideBuilder = () => {
       </div>
       <div style={{ width: "50%" }}>
         <h1>Board Tool</h1>
-
         <button onClick={() => showBuilder(true)}>Create New Board</button>
-
-        <button>Add From Collection</button>
+        <button onClick={() => showCollection(true)}>
+          Add From Collection
+        </button>
         <Dialog
           open={builder}
           maxWidth="fit-content"
@@ -63,6 +72,29 @@ const GuideBuilder = () => {
         >
           <NewBuilder type="add" showBuilder={showBuilder} />
         </Dialog>
+        <Dialog
+          open={collection}
+          maxWidth="fit-content"
+          style={{ padding: "10px" }}
+        >
+          {boardCollection &&
+            Object.keys(boardCollection).map((e) => {
+              return (
+                <div
+                  className="hover_selection"
+                  onClick={() => addExisting({ id: e, ...boardCollection[e] })}
+                >
+                  Select
+                  <h3>{boardCollection[e].title}</h3>
+                  <ViewBoard data={boardCollection[e].grid} />
+                  <h4 style={{ marginTop: "-50px" }}>
+                    notes: {boardCollection[e].subtitle}
+                  </h4>
+                </div>
+              );
+            })}
+        </Dialog>
+        {/* test */} {/* Some text */}
         {boards &&
           boards.map((e) => {
             return (
