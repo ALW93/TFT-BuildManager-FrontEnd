05f76289eacc24e33f82dddb6ebd3c6b035ec490
alycialwang@gmail.com <alycialwang@gmail.com>
2021-01-20 19:40:02 -0500

    Rehook Profile

--- src/Profile/ProfilePage.js
+++ src/Profile/ProfilePage.js
@@ -1,35 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { TFT_BASE } from "../config";
+import { useSelector } from "react-redux";
 import CheckBoxIcon from "@material-ui/icons/CheckBox";
 import "./ProfilePage.css";
 
-const ProfilePage = ({ match }) => {
-  const [user, setUser] = useState({});
-  const [buildData, setBuildData] = useState([]);
-
-  useEffect(() => {
-    const getInfo = async () => {
-      const userId = match.params.id;
-      const response = await fetch(`${TFT_BASE}/users/id/${userId}`);
-      const data = await response.json();
-      setUser(data);
-    };
-    getInfo();
-  }, [match.params.id]);
+const ProfilePage = () => {
+  const user = useSelector((state) => state.authentication.user);
 
   return (
     <div className="Profile__Container">
       <div className="profile__topbar">
         <div className={`profile__image fuwa`}></div>
         <div className="profile__details">
-          <h1>{user.username}</h1>
+          <h1>{user && user.username}</h1>
           <h2>
-            Rank: {user.rank}
-            {user.verified ? <CheckBoxIcon /> : null}
+            Rank: {user && user.rank}
+            {user && user.verified ? <CheckBoxIcon /> : null}
           </h2>
-          <h4>Joined On: {user.joined}</h4>
+          <h4>Joined On: {user && user.joined}</h4>
           <h5>
-            Followers: {user.followerCount} Following: {user.followingCount}
+            Followers: {user && user.followerCount} Following:{" "}
+            {user && user.followingCount}
           </h5>
         </div>
       </div>
