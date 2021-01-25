cf11d11a4006e632f71263fd4c54fc4424649e6c
alycialwang@gmail.com <alycialwang@gmail.com>
2021-01-25 15:43:08 -0500

    Clear Board Button Working

--- src/NewBuilder/BoardService.js
+++ src/NewBuilder/BoardService.js
@@ -1,11 +1,13 @@
 import { TFT_BASE } from "../config";
 
 export const createBoard = async (payload) => {
+  const token = window.localStorage.getItem("TOKEN_KEY");
+
   const response = await fetch(`${TFT_BASE}/boards`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      //   Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(payload),
   });
