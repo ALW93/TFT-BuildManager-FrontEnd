fef3d00cce6e7dfeacf256a96b635765218a4c5e
alycialwang@gmail.com <alycialwang@gmail.com>
2021-01-29 21:18:36 -0500

    Styled Nav

--- src/Test/Splash.js
+++ src/Test/Splash.js
@@ -3,9 +3,21 @@ import chonk from "../Assets/New/transparent_4.5.png";
 import "./Splash.css";
 import logo from "../Assets/Logo.png";
 import { InteriorSwitch } from "../Utility/routes";
+import calligraphyIcon from "../Assets/New/calligraphy.png";
 import lanternIcon from "../Assets/New/lantern.png";
+import boyIcon from "../Assets/New/boy.png";
+import dragonIcon from "../Assets/New/dragon.png";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../store/actions/authentication";
 
 const Splash = () => {
+  const user = useSelector((state) => state.authentication.user);
+  const dispatch = useDispatch();
+
+  const logoutApp = async () => {
+    await dispatch(logout());
+  };
+
   return (
     <div class="splash__background">
       <div class="login__container">
@@ -22,17 +34,19 @@ const Splash = () => {
             <p className="navLinks">Browse Builds</p>
           </div>
           <div className="flex al_c">
-            <img src={lanternIcon} className="navIcon" />
+            <img src={calligraphyIcon} className="navIcon" />
             <p className="navLinks">Builder</p>
           </div>
           <img class="login__logo" src={logo} />
           <div className="flex al_c">
-            <img src={lanternIcon} className="navIcon" />
+            <img src={boyIcon} className="navIcon" />
             <p className="navLinks">Profile</p>
           </div>
           <div className="flex al_c">
-            <img src={lanternIcon} className="navIcon" />
-            <p className="navLinks">Logout</p>
+            <img src={dragonIcon} className="navIcon" />
+            <p className="navLinks" onClick={logoutApp}>
+              Logout
+            </p>
           </div>
         </div>
         <div className="login__interior">
